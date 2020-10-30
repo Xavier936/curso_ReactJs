88d93e75245e048fa7de95b7369235774359eb47
Xavier936 <xavierlema1@gmail.com>
2020-10-30 16:59:04 -0500

    inicios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
+/*function Helloworld(props){
+  return(
+  <div id="hello">
+    <h3>{props.subtitle}</h3>
+    {props.mytext}
+  </div>
+  );
+}*/
+
+class Helloworld extends React.Component {
+  state = {
+    show: true
+  }
+
+toggleShow = () => {
+  this.setState({show: !this.state.show})
+}   
+
+  render () {
+    if (this.state.show){
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Cambiar estado</button>
+        </div>
+      );
+    }else{
+      return <h1>
+        No hay elementos
+        <button onClick={this.toggleShow}>Cambiar estadado</button>
+        </h1>
+    }
+    
+  }
+}
+
+//const App = () => <div> este es mi componente: <Helloworld/> </div>
+
+/*class App extends React.Component{
+  render(){
+    return <div>este es mi componente: <Helloworld/></div>
+  }
+}*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component: 
+      <Helloworld mytext="Hello Alexis" subtitle="loremp ipsum"/> 
+      <Helloworld mytext="que mas" subtitle="qwwet"/> 
+      <Helloworld mytext="que onda" subtitle="sxcsdc"/>
     </div>
   );
 }
